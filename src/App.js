64bec31546d534224df536f0194b0b3eda96d4bd
lwod <lwodcurrentmail@gmail.com>
2021-04-03 13:59:59 +0300

    add to the App.js handler for data from nasa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import Map from "./components/Map";
 
+
 const App = ()=>{
+    
+    const [eventData,setEventData] = useState([])
+    const [loading, setLoading] = useState(false)
+    
+    useEffect(()=>{
+        const fetchEvents = async () => {
+            setLoading(true);
+            const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+            const {events} = await res.json()
+            
+            setEventData(events)
+            setLoading(false)
+        }
+        
+        fetchEvents()
+        console.log(eventData)
+    }, [])
+    
   return (
     <div className="App">
         <Map/>
